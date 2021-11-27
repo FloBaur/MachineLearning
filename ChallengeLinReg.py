6cb2eb4d911b9f6c9b64e29c2a396789f4007e28
FloBaur <fbaur@yahoo.de>
2021-11-27 17:59:18 +0100

    Regression finished

--- ChallengeLinReg.py
+++ ChallengeLinReg.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 
-def calc_price(kilometer, m, t):
-    predPrice = m * kilometer + t
-    return predPrice
+# def calc_price(kilometer, m, t):
+#    predPrice = m * kilometer + t
+#    return predPrice
 
 
 dfCars = pd.read_csv("files/autos_prepared.csv")
@@ -16,18 +16,20 @@ kilometer = dfCars["kilometer"]
 model = LinearRegression()
 
 model.fit(dfCars[["kilometer"]], dfCars[["price"]])
-miny = min(price)
-maxy = max(price)
+minx = min(kilometer)
+maxx = max(kilometer)
 
 t = str(model.intercept_)
 m = str(model.coef_)
 
-predicted = model.predict([[miny], [maxy]])
+predicted = model.predict([[minx], [maxx]]) #betrachte diesen Bereich für die Prediction
 plt.scatter(kilometer, price)
-plt.plot([0, 50000], predicted, color="red")
+plt.plot([minx, maxx], predicted, color="red")
 plt.show()
 
-predPrice = calc_price(50000, -0.0879714, 15988.72674)
+predPrice = model.predict([[50000]])
+#predPrice = calc_price(50000, -0.0879714, 15988.72674)
 
-print('Der vorhergesagte Preis ist:')
-print(predPrice)
+test = predPrice[0][0]
+print('Der vorhergesagte Preis ist: ' + str(round(test,2)) + '€')
+
